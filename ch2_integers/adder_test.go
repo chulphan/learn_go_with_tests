8cd4b6ebd6cf677857a4153999dc4b5bdde93b34
chulphan <chkim100617@gmail.com>
2021-03-15 18:16:35 +0900

    서로 다른 두 수에 대한 덧셈에 대한 테스트를 추가하고, 실패한 뒤에 Add 함수를 리팩토링하여 제대로 된 결과를 냈다(GREEN)

--- ch2_integers/adder_test.go
+++ ch2_integers/adder_test.go
@@ -3,10 +3,21 @@ package integers
 import "testing"
 
 func TestAdder(t *testing.T) {
-	sum := Add(2, 2)
-	expected := 4
+	t.Run("2 plus 2 is 4", func(t *testing.T) {
+		sum := Add(2, 2)
+		expected := 4
 
-	if sum != expected {
-		t.Errorf("expected %d but got %d", sum, expected)
-	}
+		if sum != expected {
+			t.Errorf("expected %d but got %d", sum, expected)
+		}
+	})
+
+	t.Run("2 plus 3 is 5", func(t *testing.T) {
+		sum := Add(2, 3)
+		expected := 5
+
+		if sum != expected {
+			t.Errorf("expected %d but got %d", sum, expected)
+		}
+	})
 }
