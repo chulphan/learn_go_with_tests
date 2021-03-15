7f7f930d8c9e42700201c9d6d2a42ea9e0b398e8
chulphan <chkim100617@gmail.com>
2021-03-15 20:36:39 +0900

    현재 두 개의 테스트는 겹친다. 그러므로 하나를 제거했다

--- ch4_arrays_and_slices/sum_test.go
+++ ch4_arrays_and_slices/sum_test.go
@@ -10,15 +10,6 @@ func TestSum(t *testing.T) {
 		}
 	}
 
-	t.Run("collection of 5 number", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
-
-		got := Sum(numbers)
-		want := 15
-
-		assertEqual(t, got, want, numbers)
-	})
-
 	t.Run("collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
 
