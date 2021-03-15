19dab36d541d7555fc09bf94042036ea6d4c6182
chulphan <chkim100617@gmail.com>
2021-03-15 18:19:07 +0900

    테스트 코드의 단언부를 새로운 함수로 만드는 리팩토링 진행했다

--- ch2_integers/adder_test.go
+++ ch2_integers/adder_test.go
@@ -3,21 +3,25 @@ package integers
 import "testing"
 
 func TestAdder(t *testing.T) {
+
+	assertEquals := func(t testing.TB, sum, expected int) {
+		t.Helper()
+		if sum != expected {
+			t.Errorf("expected %d but got %d", sum, expected)
+		}
+	}
+
 	t.Run("2 plus 2 is 4", func(t *testing.T) {
 		sum := Add(2, 2)
 		expected := 4
 
-		if sum != expected {
-			t.Errorf("expected %d but got %d", sum, expected)
-		}
+		assertEquals(t, sum, expected)
 	})
 
 	t.Run("2 plus 3 is 5", func(t *testing.T) {
 		sum := Add(2, 3)
 		expected := 5
 
-		if sum != expected {
-			t.Errorf("expected %d but got %d", sum, expected)
-		}
+		assertEquals(t, sum, expected)
 	})
 }
