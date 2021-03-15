1330898295782c01f05b81b5fd08700c01486ed1
chulphan <chkim100617@gmail.com>
2021-03-15 17:51:50 +0900

    magic 문자열을 상수처리 했고, 인사말도 상수화 시켜 리팩토링했다

--- ch1_hello/hello.go
+++ ch1_hello/hello.go
@@ -6,16 +6,17 @@ import "fmt"
 	테스트를 하기 위해서는 외부의 세계(side-effect) 로부터 도메인 코드를 분리시켜야 한다
 	fmt.Println 이 side-effect 이고 우리가 전달하는 문자열은 우리의 도메인이다
 */
-
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	if language == "Spanish" {
-		return "Hola, " + name + "!"
+	if language == spanish {
+		return spanishHelloPrefix + name + "!"
 	}
 
 	return englishHelloPrefix + name + "!"
