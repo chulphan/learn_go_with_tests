67d4f4628fe0a73e45d314b5eef59168e4efabe3
chulphan <chkim100617@gmail.com>
2021-03-15 17:49:17 +0900

    기존에 Hello 함수를 쓰던 부분에 빈 문자열을 넣어서 코드와 테스트들이 정상적으로 작동되도록 했다

--- ch1_hello/hello.go
+++ ch1_hello/hello.go
@@ -13,9 +13,14 @@ func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+
+	if language == "Spanish" {
+		return "Hola, " + name + "!"
+	}
+
 	return englishHelloPrefix + name + "!"
 }
 
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", ""))
 }
