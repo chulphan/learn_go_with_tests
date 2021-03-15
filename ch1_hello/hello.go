7f7b2a0cb0fa21fdb8b065d3b7b6a2a112273419
chulphan <chkim100617@gmail.com>
2021-03-15 14:35:30 +0900

    Hello 에 대한 함수를 분리하여 테스트를 작성했다

--- ch1_hello/hello.go
+++ ch1_hello/hello.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+/*
+	테스트를 하기 위해서는 외부의 세계(side-effect) 로부터 도메인 코드를 분리시켜야 한다
+	fmt.Println 이 side-effect 이고 우리가 전달하는 문자열은 우리의 도메인이다
+*/
+
+func Hello() string {
+	return "Hello, World!"
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println(Hello())
 }
