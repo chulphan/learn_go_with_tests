0b9e33fd37f8eb2937e2ad0938890a625ce97e08
chulphan <chkim100617@gmail.com>
2021-03-15 18:02:50 +0900

    Hello 함수에서 인사말을 처리하는 부분을 새로운 함수로 추출했다. 분리함으로써 가독성이 올라갔고 역할을 나눌 수 있게 됐다

--- ch1_hello/hello.go
+++ ch1_hello/hello.go
@@ -17,16 +17,20 @@ func Hello(name string, language string) string {
 		name = "World"
 	}
 
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name + "!"
+}
 
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case korean:
 		prefix = koreanHelloPrefix
 	case spanish:
 		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
 
-	return prefix + name + "!"
+	return
 }
 
 func main() {
