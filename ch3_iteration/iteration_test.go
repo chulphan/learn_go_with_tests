b38952c6165ebd03bdb61639d16b106a2f8a1a0f
chulphan <chkim100617@gmail.com>
2021-03-15 19:16:16 +0900

    Repeat 함수가 이제 새로운 매개변수를 받아야 한다 그에 대한 테스트 코드를 작성했고 실패한다

--- ch3_iteration/iteration_test.go
+++ ch3_iteration/iteration_test.go
@@ -1,12 +1,25 @@
 package iteration
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat()
-	expected := "aaaaa"
+	t.Run("repeat 5 times a if given empty string", func(t *testing.T) {
+		repeated := Repeat("")
+		expected := "aaaaa"
 
-	if repeated != expected {
-		t.Errorf("got %q but expect %q", repeated, expected)
-	}
+		if repeated != expected {
+			t.Errorf("got %q but expect %q", repeated, expected)
+		}
+	})
+
+	t.Run("repeat 5 times given string", func(t *testing.T) {
+		repeated := Repeat("c")
+		expected := "ccccc"
+
+		if repeated != expected {
+			t.Errorf("got %q but expect %q", repeated, expected)
+		}
+	})
 }
