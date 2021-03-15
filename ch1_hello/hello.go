bd1f4eae5a01cc2e93b8de0cf3738682ccb2d520
chulphan <chkim100617@gmail.com>
2021-03-15 17:55:38 +0900

    Hello 함수에서 쓰던 magic 문자열을 상수로 변경하는 리팩토링을 진행했다 (Refactoring)

--- ch1_hello/hello.go
+++ ch1_hello/hello.go
@@ -6,9 +6,11 @@ import "fmt"
 	테스트를 하기 위해서는 외부의 세계(side-effect) 로부터 도메인 코드를 분리시켜야 한다
 	fmt.Println 이 side-effect 이고 우리가 전달하는 문자열은 우리의 도메인이다
 */
+const korean = "Korean"
 const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
+const koreanHelloPrefix = "안녕, "
 
 func Hello(name string, language string) string {
 	if name == "" {
@@ -19,8 +21,8 @@ func Hello(name string, language string) string {
 		return spanishHelloPrefix + name + "!"
 	}
 
-	if language == "Korean" {
-		return "안녕, " + name + "!"
+	if language == korean {
+		return koreanHelloPrefix + name + "!"
 	}
 
 	return englishHelloPrefix + name + "!"
