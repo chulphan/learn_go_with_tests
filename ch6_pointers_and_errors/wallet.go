652d94be5da1bff308ca3b77e6def93d0e031aa1
chulphan <chkim100617@gmail.com>
2021-03-17 19:55:55 +0900

    Balance 메소드와 Deposit 메소드를 구현했다. 하지만 Balance 에서 제대로 된 값을 출력하지 못한다. 아니면 값을 갱신 안할걸까...?

--- ch6_pointers_and_errors/wallet.go
+++ ch6_pointers_and_errors/wallet.go
@@ -1,12 +1,14 @@
 package pointers_and_errors
 
 type Wallet struct {
+	// 소문자로 썼으면 외부에 공개되지 않는 private 변수
+	balance int
 }
 
 func (w Wallet) Deposit(amount int) {
-
+	w.balance += amount
 }
 
 func (w Wallet) Balance() int {
-	return 0
+	return w.balance
 }
