027cc2f493035e557dd847cf230adfbbb1508602
chulphan <chkim100617@gmail.com>
2021-03-17 18:11:00 +0900

    넓이 함수에 대한 테스트 코드를 Table Driven Test에 맞게 리팩토링했다. 이제 중복으로 작성해야 될 코드의 양이 조금 줄었다

--- ch5_structs_methods_interfaces/shapes_test.go
+++ ch5_structs_methods_interfaces/shapes_test.go
@@ -13,26 +13,19 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
 	}
 
-	t.Run("for rectangle", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		want := 72.0
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
 
-		checkArea(t, rectangle, want)
-	})
-
-	t.Run("for circles", func(t *testing.T) {
-		circle := Circle{10}
-		want := 314.1592653589793
-
-		checkArea(t, circle, want)
-	})
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
 }
