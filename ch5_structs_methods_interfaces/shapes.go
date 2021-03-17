71c9fb8a10ea8930dbb5dd8988912320d735edbc
chulphan <chkim100617@gmail.com>
2021-03-17 18:14:47 +0900

    테스트를 통과하기 위해 Triangle(삼각형)에 대한 구조체 타입을 정의했다

--- ch5_structs_methods_interfaces/shapes.go
+++ ch5_structs_methods_interfaces/shapes.go
@@ -23,6 +23,11 @@ func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
