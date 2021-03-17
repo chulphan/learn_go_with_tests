898ad5bc5144c5d965ab032789e10233145ae7d4
chulphan <chkim100617@gmail.com>
2021-03-17 17:41:23 +0900

    기정의된 함수의 선언부 중 매개변수를 Rectangle 구조체를 받도록 변경했다. 이제 함수만 보고도 이 함수는 사각형에 대한 연산을 할 것이라고 명시한 셈이다

--- ch5_structs_methods_interfaces/shapes_test.go
+++ ch5_structs_methods_interfaces/shapes_test.go
@@ -15,7 +15,7 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 	rectangle := Rectangle{12.0, 6.0}
 	got := Area(rectangle)
-	want := 100.0
+	want := 72.0
 
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
