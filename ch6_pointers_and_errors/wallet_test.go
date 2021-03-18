bc3a1272887efb349094479c07d8758c696bf4a7
chulphan <chkim100617@gmail.com>
2021-03-18 18:27:39 +0900

    출금이 성공적으로 성공한 것에 대한 테스트 단언을 추가했고 그에 대한 헬퍼함수도 작성했다

--- ch6_pointers_and_errors/wallet_test.go
+++ ch6_pointers_and_errors/wallet_test.go
@@ -25,6 +25,14 @@ func assertError(t testing.TB, got, want error) {
 	}
 }
 
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
+	}
+}
+
 func TestWallet(t *testing.T) {
 
 	t.Run("Deposit", func(t *testing.T) {
@@ -40,11 +48,12 @@ func TestWallet(t *testing.T) {
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
 
 		want := Bitcoin(10)
 
 		assertBalance(t, wallet, want)
+		assertNoError(t, err)
 	})
 
 	t.Run("Withdraw insufficient funds", func(t *testing.T) {
