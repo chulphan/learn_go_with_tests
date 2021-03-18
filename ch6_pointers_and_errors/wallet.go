1bf1379832848cd3f2c3be7fdccbeffd52c68a49
chulphan <chkim100617@gmail.com>
2021-03-18 17:24:12 +0900

    현재 balance 는 int 로 되어있다. 우리의 의도를 더 잘 드러내기 위해 화폐단위에 대한 타입을 새로 정의하고 대체했다

--- ch6_pointers_and_errors/wallet.go
+++ ch6_pointers_and_errors/wallet.go
@@ -1,14 +1,16 @@
 package pointers_and_errors
 
+type Bitcoin int
+
 type Wallet struct {
 	// 소문자로 썼으면 외부에 공개되지 않는 private 변수
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
