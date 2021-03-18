65866f02c6000ac96b197ab76f7bb2e6dc655e78
chulphan <chkim100617@gmail.com>
2021-03-18 17:53:45 +0900

    테스트 코드에 중복되던 단언에 대한 코드를 하나의 함수로 묶었다

--- ch6_pointers_and_errors/wallet_test.go
+++ ch6_pointers_and_errors/wallet_test.go
@@ -5,18 +5,24 @@ import (
 )
 
 func TestWallet(t *testing.T) {
+
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
 		wallet.Deposit(Bitcoin(10))
 
-		got := wallet.Balance()
-
 		want := Bitcoin(10)
 
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, want)
 	})
 
 	t.Run("Withdraw", func(t *testing.T) {
@@ -24,11 +30,8 @@ func TestWallet(t *testing.T) {
 
 		wallet.Withdraw(Bitcoin(10))
 
-		got := wallet.Balance()
 		want := Bitcoin(10)
 
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, want)
 	})
 }
