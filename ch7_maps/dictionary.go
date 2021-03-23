de1877850785387405afbea68c765882348b655f
chulphan <chkim100617@gmail.com>
2021-03-23 17:37:26 +0900

    Add 함수가 제대로 동작할 수 있도록 기능을 추가했으며, 테스트도 통과하고 기능도 제대로 동작한다

--- ch7_maps/dictionary.go
+++ ch7_maps/dictionary.go
@@ -11,7 +11,16 @@ type Dictionary map[string]string
 
 // Go에서의 map 은 runtime.hmap 이고 이는 포인터값이므로 &를 사용할 필요 없다(배열, 슬라이스와 달리)
 func (d Dictionary) Add(word, definition string) error {
-	d[word] = definition
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
 	return nil
 }
 
