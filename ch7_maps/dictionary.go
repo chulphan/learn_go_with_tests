e4fc9f988bf5b2a45444e659c4a8c78d7f0bac06
chulphan <chkim100617@gmail.com>
2021-03-23 17:07:10 +0900

    Add 함수를 통해서 사전에 이미 존재하는 단어를 추가하려고 하면 에러를 내게 하고싶다. 왜냐하면 Add 함수는 이미 존재하는 값을 수정하는 역할을 하는 수가 아니기 때문이다. 그에 대한 테스트 코드를 작성했다

--- ch7_maps/dictionary.go
+++ ch7_maps/dictionary.go
@@ -6,6 +6,7 @@ var ErrNotFound = errors.New("could not find the word you were looking for")
 
 type Dictionary map[string]string
 
+// Go에서의 map 은 runtime.hmap 이고 이는 포인터값이므로 &를 사용할 필요 없다(배열, 슬라이스와 달리)
 func (d Dictionary) Add(word, definition string) {
 	d[word] = definition
 }
--- ch7_maps/dictionary_test.go
+++ ch7_maps/dictionary_test.go
@@ -3,14 +3,28 @@ package maps
 import "testing"
 
 func testAdd(t *testing.T) {
-	dictionary := Dictionary{}
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
 
-	word := "test"
-	definition := "this is just a test"
+		word := "test"
+		definition := "this is just a test"
 
-	dictionary.Add(word, definition)
+		err := dictionary.Add(word, definition)
 
-	assertDefinition(t, dictionary, word, definition)
+		assertError(err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
 }
 
 func TestSearch(t *testing.T) {
