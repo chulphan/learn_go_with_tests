399bebc0b029e11d67c1b4ecce28994ab12be32f
chulphan <chkim100617@gmail.com>
2021-03-23 18:15:44 +0900

    기존에 존재하는 단어에 대한 정의를 수정하는 요구사항이 추가됐다. 그에 대한 테스트 코드를 작성했다

--- ch7_maps/dictionary_test.go
+++ ch7_maps/dictionary_test.go
@@ -27,6 +27,19 @@ func TestAdd(t *testing.T) {
 	})
 }
 
+func TestUpdate(t *testing.T) {
+	word := "test"
+	definition := "this is just a test"
+
+	dictionary := Dictionary{word: definition}
+
+	newDefinition := "new definition"
+
+	dictionary.Update(word, newDefinition)
+
+	assertDefinition(t, dictionary, word, newDefinition)
+}
+
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
