ee7727aead264109f84adab5dfedc33abd7702e2
chulphan <chkim100617@gmail.com>
2021-03-23 18:24:30 +0900

    Update 함수는 새로운 단어를 추가할 수 없다. 그에 대한 테스트 코드를 작성했다

--- ch7_maps/dictionary_test.go
+++ ch7_maps/dictionary_test.go
@@ -28,16 +28,30 @@ func TestAdd(t *testing.T) {
 }
 
 func TestUpdate(t *testing.T) {
-	word := "test"
-	definition := "this is just a test"
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
 
-	dictionary := Dictionary{word: definition}
+		dictionary := Dictionary{word: definition}
 
-	newDefinition := "new definition"
+		newDefinition := "new definition"
 
-	dictionary.Update(word, newDefinition)
+		err := dictionary.Update(word, newDefinition)
 
-	assertDefinition(t, dictionary, word, newDefinition)
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, newDefinition)
+	})
+
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+
+		dictionary := Dictionary{}
+
+		err := dictionary.Update(word, definition)
+
+		assertError(t, err, ErrWordDoesNotExist)
+	})
 }
 
 func TestSearch(t *testing.T) {
